c4ff5820730ce95f19121332637179900bf5f031
mrilikecoding <nathanrgreen@gmail.com>
2014-06-10 18:51:01 -0500

    updated video loading

--- app/assets/javascripts/manifests/home_manifest.js
+++ app/assets/javascripts/manifests/home_manifest.js
@@ -2,7 +2,6 @@
 //= require vendor/jquery-ui.min
 //= require vendor/bootstrap.min
 //= require vendor/video
-//= require vendor/modernizr.custom.72241
 //= require vendor/imagesloaded.pkgd.min
 //= require vendor/bigvideo
 //= require modules/home
