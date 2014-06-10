f1fc4b5f32c815d937404880e245e1fa18556cd6
mrilikecoding <nathanrgreen@gmail.com>
2014-06-10 15:32:31 -0500

    added mandrill for mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,16 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :port =>           '587',
+      :address =>        'smtp.mandrillapp.com',
+      :user_name =>      ENV['MANDRILL_USERNAME'],
+      :password =>       ENV['MANDRILL_APIKEY'],
+      :domain =>         'heroku.com',
+      :authentication => :plain
+  }
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
